a64b42a8176bceae1b164c3c1494cf9b0688ebf8
Logan Van Hook <loganvan@att.net>
2020-07-22 11:28:53 -0400

    D2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,7 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
         self.running = False
@@ -18,26 +18,21 @@ class CPU:
     def ram_write(self, val, ind):
         self.reg[ind] = val
 
-    def load(self):
+    def load(self, fileinc):
         """Load a program into memory."""
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            address = 0
+            with open(fileinc) as file:
+                for line in file:
+                    command = line.split("#")[0].strip()
+                    if command == '':
+                        continue
+                    self.ram[address] = int(command, 2)
+                    address += 1
+        
+        except FileNotFoundError:
+            sys.exit()
 
 
     def alu(self, op, reg_a, reg_b):
@@ -46,6 +41,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -74,6 +71,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         self.running = True
 
@@ -87,5 +85,7 @@ class CPU:
                 print(f"{value} at {self.ram[self.pc + 1]}")
             if command == HLT:
                 self.running = False
+            if command == MUL:
+                self.alu("MUL", self.ram[self.pc + 1], self.ram[self.pc + 2])
             
             self.pc += 1 + (command >> 6)
