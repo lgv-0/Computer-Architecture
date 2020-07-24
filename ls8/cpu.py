a77f369ccde88f024f5d3cac02211dc448952920
Logan Van Hook <loganvan@att.net>
2020-07-24 13:25:35 -0400

    D5

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -43,6 +43,13 @@ class CPU:
         #elif op == "SUB": etc
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.flag = 0b00000001
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.flag = 0b10000010
+            else:
+                self.flag = 0b00000100
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -75,6 +82,10 @@ class CPU:
         POP = 0b01000110
         CALL = 0b01010000
         RET = 0b00010001
+        CMP = 0b10100111
+        JMP = 0b01010100
+        JEQ = 0b01010101
+        JNE = 0b01010110
 
         self.running = True
 
@@ -82,26 +93,25 @@ class CPU:
             command = self.ram[self.pc]
             if command == LDI:
                 self.ram_write(self.ram[self.pc + 2], self.ram[self.pc + 1])
-                print(f"Writing {self.ram[self.pc + 2]} to {self.ram[self.pc + 1]}")
-            if command == PRN:
+            elif command == PRN:
                 value = self.ram_read(self.ram[self.pc + 1])
                 print(f"{value} at {self.ram[self.pc + 1]}")
-            if command == HLT:
+            elif command == HLT:
                 self.running = False
-            if command == MUL:
+            elif command == MUL:
                 self.alu("MUL", self.ram[self.pc + 1], self.ram[self.pc + 2])
-            if command == PUSH:
+            elif command == PUSH:
                 self.reg[7] -= 1
                 sp = self.reg[7]
                 reg_index = self.ram[self.pc + 1]
                 self.ram[sp] = self.reg[reg_index]
-            if command == POP:
+            elif command == POP:
                 sp = self.reg[7]
                 popped_value = self.ram[sp]
                 reg_index = self.ram[self.pc + 1]
                 self.reg[reg_index] = popped_value
                 self.reg[7] += 1
-            if command == CALL:
+            elif command == CALL:
                 reg_idx = self.ram[self.pc + 1]
                 jump_address = self.reg[reg_idx]
                 return_address = self.pc + 2
@@ -110,11 +120,33 @@ class CPU:
                 self.ram[sp] = return_address
                 self.pc = jump_address
                 continue
-            if command == RET:
+            elif command == RET:
                 sp = self.reg[7]
                 return_address = self.ram[sp]
                 self.reg[7] += 1
                 self.pc = return_address
                 continue
+            elif command == CMP:
+                self.alu("CMP", self.ram[self.pc + 1], self.ram[self.pc + 2])
+            elif command == JMP:
+                jump_address = self.reg[self.ram[self.pc + 1]]
+                self.pc = jump_address
+                continue
+            elif command == JEQ:
+                if self.flag is None:
+                    # W h a t ?
+                    print("What messed up ? ? ?")
+                elif self.flag == 0b00000001:
+                    jump_address = self.reg[self.ram[self.pc + 1]]
+                    self.pc = jump_address
+                    continue
+            elif command == JNE:
+                if self.flag is None:
+                    # W h a t ?
+                    print("What messed up ? ? ?")
+                elif self.flag != 0b00000001:
+                    jump_address = self.reg[self.ram[self.pc + 1]]
+                    self.pc = jump_address
+                    continue
             
             self.pc += 1 + (command >> 6)
